7cb72d0d0f397ded88d3e53fad57d4d21ca5d021
Dan Finn <dfinn@plansource.com>
2019-10-15 23:29:26 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+A simple webapp that takes an integer and a character as input and builds a pyramid out of it.  For example, 5 and "a" would get you:
+
+a
+aa
+aaa
+aaaa
+aaaaa
+aaaa
+aaa
+aa
+a
+
+This is built using python + flask + docker + docker-compose
---
+++ app/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# Basic flask app to serve hello world and keep a connection count in redis
+
+from flask import Flask, request, render_template
+from redis import Redis
+import os
+
+app = Flask(__name__)
+redis = Redis(host="redis", port=6379)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def create_pyramid():
+    if request.method == 'POST':
+        character = request.form.get('char')
+        number = int(request.form.get('num'))
+        hits = redis.get('hits').decode('utf-8')
+
+        return render_template('pyramid.html', number = number, character = character, hits = hits)
+
+    redis.incr('hits')
+    return '''<form method="POST">
+		Character: <input type="text" name="char"><br>
+		Number: <input type="text" name="num"><br>
+		<input type="submit" value="Submit"><br>
+           </form>'''
+
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
