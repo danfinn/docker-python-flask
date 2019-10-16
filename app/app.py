4977cc09dca9630ef086035c727e3b3981437e77
Dan Finn <dfinn@plansource.com>
2019-10-16 00:26:49 -0600

    using a template for the input form

--- app/app.py
+++ app/app.py
@@ -4,7 +4,6 @@
 
 from flask import Flask, request, render_template
 from redis import Redis
-#import os
 
 app = Flask(__name__)
 redis = Redis(host="redis", port=6379)
@@ -13,18 +12,16 @@ redis = Redis(host="redis", port=6379)
 @app.route('/', methods=['GET', 'POST'])
 def create_pyramid():
     if request.method == 'POST':
+
         character = request.form.get('char')
         number = int(request.form.get('num'))
         hits = redis.get('hits').decode('utf-8')
 
         return render_template('pyramid.html', number = number, character = character, hits = hits)
 
+    # it's a GET request, increment the page counter and display the input form
     redis.incr('hits')
-    return '''<form method="POST">
-		Character: <input type="text" name="char"><br>
-		Number: <input type="text" name="num"><br>
-		<input type="submit" value="Submit"><br>
-           </form>'''
+    return render_template('form.html')
 
 
 if __name__ == '__main__':
